ab4c57fc88ded49ef621f45f8235bd9b5af3f2f7
Gomdi <ginpa0886@gmail.com>
2023-03-20 10:29:45 +0900

    🎉Init Dong_Project_A

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                GameObject go = new GameObject(typeof(T).ToString());
+                _instance = go.AddComponent<T>();
+                DontDestroyOnLoad(go);
+            }
+            return _instance;
+        }
+    }
+}
